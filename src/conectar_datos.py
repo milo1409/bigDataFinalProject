ad2753ebdd6ef86059427ee170b2f9542af8b79a
cporras <cporras@esri.co>
2025-12-13 13:19:28 -0500

    cporras

---
+++ src/conectar_datos.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import pyspark
+from pyspark.sql import SparkSession
+import pyspark.sql.functions as F
+from pyspark.sql.types import *
+from pyspark.sql.window import Window
+from pyspark.sql import DataFrame
+import os
+
+
+class extraer_datos_procesamiento():
+
+    def conectar_datos():
+        pass
+
+    def generar_formato_parquet():
+        pass
