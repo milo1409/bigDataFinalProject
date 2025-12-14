4dbe8db3b93f5d4c9821c640113806e03c645382
cporras <cporras@esri.co>
2025-12-13 19:55:14 -0500

    ajustes

--- src/conectar_datos.py
+++ src/conectar_datos.py
@@ -1,24 +1,30 @@
+import os
 from typing import Optional
-from pyspark.sql import DataFrame, SparkSession
+import pandas as pd
+from pyspark.sql import DataFrame as SparkDataFrame
 
 
 class ExtraerDatosProcesamiento:
-    def __init__(self, spark: SparkSession, utils, config: dict):
+    def __init__(self, spark, utils, config: dict):
         self.spark = spark
         self.Utils = utils
         self.config = config
 
-    def generar_formato_parquet(self,df: DataFrame,mode: str = "overwrite") -> str:
+    def generar_formato_parquet(self, df, mode: str = "overwrite") -> str:
+   
+        if isinstance(df, pd.DataFrame):
+            df_spark = self.spark.createDataFrame(df)
+        elif isinstance(df, SparkDataFrame):
+            df_spark = df
+        else:
+            raise TypeError("df debe ser un pandas.DataFrame o un pyspark.sql.DataFrame")
 
-        salida_relativa = self.config.get("data_procesada")
-        if salida_relativa is None:
-            raise ValueError("La clave 'data_procesada' no existe en el config.")
+        rel_path = self.config.get("data_procesada")
+        if rel_path is None:
+            raise ValueError("Falta la clave 'data_procesada' en config")
 
-        salida_absoluta = self.Utils.resolve_path(
-            salida_relativa,
-            base_path=self.config.get("base_path")
-        )
+        salida = self.Utils.resolve_path(rel_path, base_path=self.config.get("base_path"))
 
-        df.write.mode(mode).parquet(salida_absoluta)
+        df_spark.write.mode(mode).parquet(salida)
 
-        return salida_absoluta
+        return salida
